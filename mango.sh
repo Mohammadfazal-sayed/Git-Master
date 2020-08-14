84b5dfffc1374f4d4ea5705b05c60ad1b023ad94
Mohammadfazal-sayed <fazal92.sayed@gmail.com>
2020-08-14 12:29:05 +0530

    123456789

--- mango.sh
+++ mango.sh
@@ -1,2 +1,2 @@
 nfv fdkjkfdjdcmnkjfdndfjkddddddddddddmncvm cncvm,nm,xcnmfncvnjfdnvm,ddnnsdfv jfdsvnjf,hhjhjjhhjjnb,fgjhgfj.123456789+7788000000123***********
-!@@@@@@ 12345,Apple
+!@@@@@@ 12345,Apple1
